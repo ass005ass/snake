e92bd3cf2562f364d8133fd6649dd14be0e090ea
ass005ass <ilja05as@yandex.ru>
2017-05-12 22:06:24 +0400

    Научили змею поедать пищу.

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -32,16 +32,31 @@ namespace Snake
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Drow();
 
+
+            FoodCreator foodCreator = new FoodCreator(80, 25, '$');
+            Point food = foodCreator.CreateFood();
+            food.Drow();
+
             while (true)
             {
+                if (snake.Eat( food ) )
+                {
+                    food = foodCreator.CreateFood();
+                    food.Drow();
+                }
+                else
+                {
+                    snake.Move();
+                }
+
+                Thread.Sleep(100);
+
                 if (Console.KeyAvailable)
                 {
                     ConsoleKeyInfo key = Console.ReadKey();
                     snake.HandleKey(key.Key);
                 }
 
-                Thread.Sleep(100);
-                snake.Move();
             }
         }
     }
